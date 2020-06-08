b6d6abcc4ca511a600188ac69a371db9b0b56748
Leon J <leon@Leons-iMac.broadband>
2020-06-08 17:54:35 +0100

    Updates

---
+++ precache-manifest.f9bf64f6762498a6368183365fc41031.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "816590861f7f98321c6cb00ad07e3913",
+    "url": "/touchtight-react/index.html"
+  },
+  {
+    "revision": "39eda758aeac62633a8a",
+    "url": "/touchtight-react/static/css/main.b788277e.chunk.css"
+  },
+  {
+    "revision": "bb2ea894ba7b64a5bca8",
+    "url": "/touchtight-react/static/js/2.2ca497d4.chunk.js"
+  },
+  {
+    "revision": "39eda758aeac62633a8a",
+    "url": "/touchtight-react/static/js/main.34bffeed.chunk.js"
+  },
+  {
+    "revision": "f78bcd47126730264b42",
+    "url": "/touchtight-react/static/js/runtime~main.82899fd8.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/touchtight-react/static/media/crown.ed28accb.svg"
+  }
+]);
